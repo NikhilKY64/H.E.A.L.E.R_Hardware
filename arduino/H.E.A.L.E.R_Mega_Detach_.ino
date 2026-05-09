25f078d50c70b6fd238a502494d798a914877e91
Nikhil Kumar Yadav <142158924+NikhilKY64@users.noreply.github.com>
2026-05-09 21:47:08 +0530

    Tuned servos angle

--- arduino/H.E.A.L.E.R_Mega_Detach_.ino
+++ arduino/H.E.A.L.E.R_Mega_Detach_.ino
@@ -17,8 +17,9 @@
 // Individual Angles for each servo [1, 2, 3, 4, FA]
 // If a door moves the WRONG way, just swap the OPEN and CLOSE numbers for that door!
 // [Door 1, Door 2, Door 3, Door 4, First Aid]
-const int OPEN_ANGLES[]  = {40, 40, 155, 155, 40}; //
-const int CLOSE_ANGLES[] = {155, 155, 40, 40, 155};
+const int OPEN_ANGLES[]  = {34, 35, 35, 42, 40};
+const int CLOSE_ANGLES[] = {153, 150, 152, 157, 155};
+
 
 const int BAUD_RATE = 9600;
 unsigned long lastRFIDCheck = 0;
